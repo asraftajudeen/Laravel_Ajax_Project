754eabba56272e4a95409429c41ab4504ddd4ebb
Asraf Tajudeen <asraftajudeen3@gmail.com>
2024-07-29 22:18:55 +0530

    Your commit message here

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TodoController;
+use App\Models\Link;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+Route::get('/', [TodoController::class,'index']);
+Route::post('/add', [TodoController::class,'add']);
+Route::post('/update/{id}', [TodoController::class,'update']);
+Route::get('delete/{id}',[TodoController::class,'delete']);
+Route::get('/done/{id}', [TodoController::class,'done']);
